b217a23800c65b7a22099f71d319d70447a6f97f
Joel <joel.m.hayward@gmail.com>
2022-05-30 14:17:47 +1200

    answer switches between light and dark mode

--- app.js
+++ app.js
@@ -5,11 +5,13 @@ function switchMode() {
   if (lightMode) {
     document.getElementById("text").innerHTML = "Switch to Light Mode";
     document.getElementById("text").style.color = "white";
+    document.getElementById("display").style.color = "white";
     document.body.style.background = "black";
     lightMode = false;
   } else {
     document.getElementById("text").innerHTML = "Switch to Dark Mode";
     document.getElementById("text").style.color = "black";
+    document.getElementById("display").style.color = "black";
     document.body.style.background = "white";
     lightMode = true;
   }
